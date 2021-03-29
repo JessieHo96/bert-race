4a9a8e9fedac091c456240968a6f9ed64d0a74af
JessieHo96 <hexiaofen96@gmial.com>
2021-03-29 20:18:05 +0800

    add script

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
-python race.py --data_dir=RACE --bert_model=bert-large-uncased --output_dir=large_models --max_seq_length=320 --do_train --do_eval  --do_lower_case --train_batch_size=8 --eval_batch_size=32 --learning_rate=1e-5 --num_train_epochs=2 --gradient_accumulation_steps=8 --loss_scale=0
-
-
+python race.py\
+ --data_dir=/content/RACE \
+ --bert_model=bert-base-uncased \
+ --output_dir=large_models \
+ --max_seq_length=256 \
+ --do_train \
+ --do_eval  \
+ --do_lower_case \
+ --train_batch_size=4 \
+ --eval_batch_size=4 \
+ --learning_rate=1e-5 \
+ --num_train_epochs=2 \
+ --gradient_accumulation_steps=1 \
+ --loss_scale=0
