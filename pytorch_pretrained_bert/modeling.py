9429f442c24a85c5b3793d5f6568b1a713824451
JessieHo96 <hexiaofen96@gmial.com>
2021-03-29 20:22:58 +0800

    update model

--- pytorch_pretrained_bert/modeling.py
+++ pytorch_pretrained_bert/modeling.py
@@ -1629,3 +1629,67 @@ class BertForQuestionAnswering(BertPreTrainedModel):
             return total_loss
         else:
             return start_logits, end_logits
+        
+class BertForQuestionAnsweringSeqSCAVPooler(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertForQuestionAnsweringSeqSCAVPooler, self).__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config)
+        self.attention = SCAttention(config.hidden_size, config.hidden_size)
+        self.qa_outputs = nn.Linear(config.hidden_size, config.num_labels)
+        
+        self.has_ans = nn.Sequential(nn.Dropout(p=config.hidden_dropout_prob), nn.Linear(config.hidden_size, 2))
+
+        self.init_weights()
+
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, pq_end_pos=None, position_ids=None, head_mask=None,
+                inputs_embeds=None, start_positions=None, end_positions=None, is_impossibles=None):
+
+        outputs = self.bert(input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids,
+                            position_ids=position_ids,
+                            head_mask=head_mask,
+                            inputs_embeds=inputs_embeds)
+
+        sequence_output = outputs[0]
+
+        query_sequence_output, context_sequence_output, query_attention_mask, context_attention_mask = \
+            split_ques_context(sequence_output, pq_end_pos)
+        sequence_output = self.attention(sequence_output, query_sequence_output, query_attention_mask)
+
+        sequence_output = sequence_output + outputs[0]
+
+        logits = self.qa_outputs(sequence_output)
+        start_logits, end_logits = logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+        
+        first_word = sequence_output[:, 0, :]
+        has_log = self.has_ans(first_word)
+        
+        
+        outputs = (start_logits, end_logits, has_log,) + outputs[2:]
+        
+        if start_positions is not None and end_positions is not None:
+            # If we are on multi-GPU, split add a dimension
+            if len(start_positions.size()) > 1:
+                start_positions = start_positions.squeeze(-1)
+            if len(end_positions.size()) > 1:
+                end_positions = end_positions.squeeze(-1)
+            # sometimes the start/end positions are outside our model inputs, we ignore these terms
+            ignored_index = start_logits.size(1)
+            start_positions.clamp_(0, ignored_index)
+            end_positions.clamp_(0, ignored_index)
+
+            loss_fct = CrossEntropyLoss(ignore_index=ignored_index)
+            start_loss = loss_fct(start_logits, start_positions)
+            end_loss = loss_fct(end_logits, end_positions)
+            
+            choice_loss = loss_fct(has_log, is_impossibles)
+            total_loss = (start_loss + end_loss + choice_loss) / 3
+            
+            outputs = (total_loss,) + outputs
+
+        return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
+
